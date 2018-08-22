8d59206a214e22a8e6f98072633a6e6d8aeb8e12
Yurii Koval <13747753+kovalj@users.noreply.github.com>
2018-08-22 16:11:38 +0300

    some more changes to classes

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ class Art:
         self.owner = owner
 
     def __repr__(self):
-        return 'Monet, Claude. "Vétheuil in the Fog". 1879, oil on canvas.'
+        return '%s. "%s". %s, %s. %s, %s.' % (self.artist, self.title, 
+                                            self.year, self.medium,
+                                            self.owner.name, self.owner.location)
 
 class Marketplace:
     def __init__(self):
@@ -18,35 +20,39 @@ class Marketplace:
     def add_listing(self, new_listing):
         self.listings.append(new_listing)
 
-    def remove_listing(self, remove_listing):
-        self.listings.remove(remove_listing)
+    def remove_listing(self, expired_listing):
+        self.listings.remove(expired_listing)
 
     def show_listings(self):
-        for item in self.listings:
-            print('{0}. "{1}". {2}, {3}. {4}, {5}.'.format(item.artist, 
-                                                        item.title,
-                                                        item.year,
-                                                        item.medium,
-                                                        item.owner.name,
-                                                        item.location))  
+        for listing in self.listings:
+            print('Martketplace item : {0}'.format(listing))
+        else: 
+            print("The Marketplace is empty!")
 
 class Client:
     def __init__(self, name, location, is_museum):
         self.name = name
-        self.location = location
         self.is_museum = is_museum
+        if is_museum:
+            self.location = location
+        else:
+            self.location = 'Private Collection'
     
     def sell_artwork(self, artwork, price):
         if artwork.owner == self:
-            Listing(artwork, price, artwork.owner)
-            veneer.add_listing(artwork)
+            new_listing = Listing(artwork, price, self)
+            veneer.add_listing(new_listing)
 
     def buy_artwork(self, artwork):
         if artwork.owner != self:
-            if artwork in veneer.listings:
-                art_listing = artwork
-                artwork.owner = self
-                veneer.remove_listing(artwork)
+            art_listing = None
+            for listing in veneer.listings:
+                if listing.art == artwork:
+                    art_listing = listing
+                    break
+        elif art_listing != None:
+            art_listing.art.owner = self
+            veneer.remove_listing(art_listing)
   
     # TODO: Add a wallet instance variable to clients, update the buying and 
     # selling of artworks to also exchange dollar amounts.
@@ -61,17 +67,18 @@ class Listing:
         self.stellar = stellar
 
     def __repr__(self):
-        return 'Monet, Claude. "Vétheuil in the Fog", 1879. $1M'
+        return '%s. %s.' % (self.art.title, self.price)
 
     # TODO: Create expiration dates for listings! Have out of date listings
     #  automatically removed from the marketplace.
 
 # Create the Marketplace. 
 veneer = Marketplace()
+
 veneer.show_listings()
 
 # Add a client.
-edytta = Client(name='Edytta Halpirt', location='Private Collection', is_museum=False)
+edytta = Client(name='Edytta Halpirt', location=None, is_museum=False)
 
 # Add an artwork.
 girl_with_mandolin = Art(artist='Picasso, Pablo', 
@@ -79,28 +86,18 @@ girl_with_mandolin = Art(artist='Picasso, Pablo',
                         medium='oil on canvas', 
                         year=1910, owner=edytta)
 
-print('{0}. "{1}". {2}, {3}. {4}, {5}.'.format(girl_with_mandolin.artist, 
-                                    girl_with_mandolin.title,
-                                    girl_with_mandolin.year, 
-                                    girl_with_mandolin.medium,
-                                    girl_with_mandolin.owner.name,
-                                    girl_with_mandolin.owner.location))
+print(girl_with_mandolin)
 
 mona = Client(name='The MOMA', location='New York', is_museum=True)
 
 # Sell an artwork.
-edytta.sell_artwork(girl_with_mandolin, "$6M")
+edytta.sell_artwork(girl_with_mandolin, "$6M (USD)")
 
 veneer.show_listings()
 
 # Buy an artwork.
 mona.buy_artwork(girl_with_mandolin)
 
-print('{0}. "{1}". {2}, {3}. {4}, {5}.'.format(girl_with_mandolin.artist, 
-                                    girl_with_mandolin.title,
-                                    girl_with_mandolin.year, 
-                                    girl_with_mandolin.medium,
-                                    girl_with_mandolin.owner.name,
-                                    girl_with_mandolin.owner.location))
+print(girl_with_mandolin)
 
 veneer.show_listings()
